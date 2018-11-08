ed25c36d6dc253ec056ab724d477add5656d3722
김수민 <kimsm23236@naver.com>
2018-11-08 18:15:41 +0900

    PP02.BreakUpHelloSDL

--- GameFrameWork/PP01.HelloSDL/main.cpp
+++ GameFrameWork/PP01.HelloSDL/main.cpp
@@ -3,30 +3,57 @@
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-int main(int argc, char* args[])
+bool init(const char*, int, int, int, int, int);
+void render();
+void update() { }
+void handleEvents() { }
+void clean() { }
+
+bool g_bRunning = true;
+
+int main(int argc, char* argv[])
 {
-	
+	if (init("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
+	}
+	else
+	{
+		return 1;
+	}
+	while (g_bRunning)
+	{
+		render();
+	}
+	SDL_Quit();
+
+	return 0;
+}
+
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
+{
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		g_pWindow = SDL_CreateWindow("PP01.HelloSDL",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
 		if (g_pWindow != 0)
 		{
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-		else
-		{
-			return 1;
-		}
-
-		SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-		SDL_RenderClear(g_pRenderer);
-		SDL_RenderPresent(g_pRenderer);
-		SDL_Delay(5000);
-		SDL_Quit();
-		return 0;
+		}		
 	}
+	else
+	{
+		return false;	// sdl could not initialize
+	}
+	return true;
+}
+
+void render()
+{
+	// set to black
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	// clear the window to black
+	SDL_RenderClear(g_pRenderer);
+	// show the window
+	SDL_RenderPresent(g_pRenderer);
 }
