10de7a8d2ea7fd4e38c00d29b195beb0e723e61c
김수민 <kimsm23236@naver.com>
2018-11-15 15:58:04 +0900

    PP07.TextureManager

--- GameFrameWork/PP01.HelloSDL/Game.h
+++ GameFrameWork/PP01.HelloSDL/Game.h
@@ -1,4 +1,8 @@
+#include <iostream>
+#include <SDL_image.h>
 #include "SDL.h"
+#include "TextureManager.h"
+
 
 class Game
 {
@@ -15,8 +19,11 @@ public:
 private:
 	SDL_Window * m_pWindow;
 	SDL_Renderer* m_pRenderer;
-	SDL_Texture* m_pTexture;			// the new SDL_Texture variable
-	SDL_Rect m_sourceRectangle;			// 원본 사각형
-	SDL_Rect m_destinationRectangle;	// 대상 사각형
+	// SDL_Texture* m_pTexture;			// the new SDL_Texture variable
+	// SDL_Rect m_sourceRectangle;			// 원본 사각형
+	// SDL_Rect m_destinationRectangle;	// 대상 사각형
+
+	int m_currentFrame;
+	TextureManager m_textureManager;
 	bool m_bRunning;
 };
