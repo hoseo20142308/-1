4c0692f65325e482a764e3f44b4a0aa682460f69
김수민 <kimsm23236@naver.com>
2018-11-15 17:45:57 +0900

    PP10.Polymorphism

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -27,9 +27,6 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 			return false;
 		}
 
-		m_go.load(100, 100, 128, 82, "animate");
-		m_player.load(300, 300, 128, 82, "animate");
-
 		//m_textureManager.load("Assets/animate-alpha.png", "animate", m_pRenderer);
 
 		//m_sourceRectangle.w = 128;
@@ -40,7 +37,17 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		//m_destinationRectangle.w = m_sourceRectangle.w;
 		//m_destinationRectangle.h = m_sourceRectangle.h;
 
+		m_go = new GameObject();
+		m_player = new Player();
+		m_enemy = new Enemy();
 
+		m_go->load(100, 100, 128, 82, "animate");
+		m_player->load(300, 300, 128, 82, "animate");
+		m_enemy->load(0, 0, 128, 82, "animate");
+
+		m_gameObjects.push_back(m_go);
+		m_gameObjects.push_back(m_player);
+		m_gameObjects.push_back(m_enemy);
 	}
 	else
 	{
@@ -68,16 +75,26 @@ void Game::render()
 	//TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82, m_pRenderer);
 	//TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
 
-	m_go.draw(m_pRenderer);
-	m_player.draw(m_pRenderer);
+	//m_go.draw(m_pRenderer);
+	//m_player.draw(m_pRenderer);
+
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->draw(m_pRenderer);
+	}
 
 	SDL_RenderPresent(m_pRenderer);	// 화면 제시하기
 }
 
 void Game::update()
 {
-	m_go.update();
-	m_player.update();
+	//m_go.update();
+	//m_player.update();
+
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->update();
+	}
 }
 
 void Game::clean()
--- GameFrameWork/PP01.HelloSDL/Game.h
+++ GameFrameWork/PP01.HelloSDL/Game.h
@@ -4,6 +4,8 @@
 #include "TextureManager.h"
 #include "GameObject.h"
 #include "Player.h"
+#include "Enemy.h"
+#include <vector>
 
 
 class Game
@@ -20,14 +22,19 @@ public:
 	bool running() { return m_bRunning; }
 private:
 
-	GameObject m_go;
-	Player m_player;
+	
 
 	SDL_Window * m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	// SDL_Texture* m_pTexture;			// the new SDL_Texture variable
 	// SDL_Rect m_sourceRectangle;			// 원본 사각형
 	// SDL_Rect m_destinationRectangle;	// 대상 사각형
+
+	std::vector<GameObject*> m_gameObjects;
+
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
 
 	int m_currentFrame;
 	TextureManager* m_textureManager;
