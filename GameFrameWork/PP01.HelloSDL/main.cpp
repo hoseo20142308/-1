7d93c386b03f61da1c913d9fb9f9a54081b9665d
김수민 <kimsm23236@naver.com>
2018-11-15 18:30:54 +0900

    PP12.Vector2D

--- GameFrameWork/PP01.HelloSDL/main.cpp
+++ GameFrameWork/PP01.HelloSDL/main.cpp
@@ -1,10 +1,15 @@
 #include <SDL.h>
 #include "Game.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main(int argc, char* argv[])
 {
 	//g_game = new Game();
 	//g_game->init("Chapter 1", 100, 100, 640, 480, false);
+
+	Uint32 frameStart, frameTime;
 
 	std::cout << "Game init attempt...\n";
 	if (TheGame::Instance()->init("Chapter 1", 100, 100, 640, 480, false))
@@ -12,10 +17,18 @@ int main(int argc, char* argv[])
 		std::cout << "Game init success!\n";
 		while (TheGame::Instance()->running())
 		{
+			frameStart = SDL_GetTicks();
+
 			TheGame::Instance()->handleEvents();
 			TheGame::Instance()->update();
 			TheGame::Instance()->render();
-			SDL_Delay(10);
+
+			frameTime = SDL_GetTicks() - frameStart;
+
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
 		}
 	}
 	else
