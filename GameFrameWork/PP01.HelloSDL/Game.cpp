f69f3b110fca84fbe927845bb9bb308a024d044f
김수민 <kimsm23236@naver.com>
2018-11-15 15:23:21 +0900

    PP05.AnimatingSprite

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -1,50 +1,58 @@
 #include "Game.h"
+#include <iostream>
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
 
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
 
-		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+		m_bRunning = true;
+
+		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 
 		SDL_FreeSurface(pTempSurface);
 
-		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
 
-		m_bRunning = true;
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
+
+
 	}
 	else
 	{
-		m_bRunning = false;
 		return false;	// sdl could not initialize
 	}
-	
+
+
+
 	return true;
 }
 
 void Game::render()
 {
-	// 원본 사각형 : m_sourceRectangle
-	// 대상 사각형 : m_destinationRectangle
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
 
 	// clear the renderer to the draw color
 	SDL_RenderClear(m_pRenderer);	// draw color로 render 지우기
-
-	// 원본 사각형과 대상 사각형의 위치와 크기에 따라 화면에 다르게 나타남
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
-
+									// 원본 사각형과 대상 사각형의 위치와 크기에 따라 화면에 다르게 나타남...
+	SDL_RenderCopy(m_pRenderer, m_pTexture,
+		&m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);	// 화면 제시하기
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
 
 void Game::clean()
@@ -64,6 +72,9 @@ void Game::handleEvents()
 		{
 		case SDL_QUIT:
 			m_bRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			m_destinationRectangle.x++;
 			break;
 		default:
 			break;
